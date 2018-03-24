1a99b4b1c98d67b3262c26bae780b38d993048c9
Yuanhao Tian <hlju.tyh@gmail.com>
2018-03-25 00:37:28 +0800

    add some doc and finish HW2.

--- HW2/LogAnalysis.hs
+++ HW2/LogAnalysis.hs
@@ -19,18 +19,29 @@ parse = map parseMessage . lines
 -- Exercise 2
 
 insert :: LogMessage -> MessageTree -> MessageTree
-
+insert log Leaf         = Node Leaf log Leaf
+insert (Unknown _) tree = tree
+insert x (Node left root right) 
+    | (getTimeStamp x) < (getTimeStamp root) = Node (insert x left) root right
+    | (getTimeStamp x) > (getTimeStamp root) = Node left root (insert x right)
+    where getTimeStamp (LogMessage _ timestamp _)  = timestamp
 
 -- Exercise 3
 
 build :: [LogMessage] -> MessageTree
-
+build = foldr insert Leaf
 
 -- Exercise 4
 
 inOrder :: MessageTree -> [LogMessage]
+inOrder Leaf = []
+inOrder (Node left root right) = (inOrder left) ++ [root] ++ (inOrder right)
 
 
 -- Exercise 5 
 
 whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong = map getMessage . inOrder . build . filter isWrong
+    where isWrong (LogMessage (Error severity) _ _) = severity >= 50
+          isWrong _                                 = False
+          getMessage (LogMessage _ _ msg)           = msg
